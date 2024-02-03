8668f6df38d9171452bb186182d158159245d08e
deelaka99 <123wejith@gmail.com>
2024-02-03 14:06:33 +0530

    created medication routes

--- src/routes/medication.js
+++ src/routes/medication.js
@@ -1,1 +1,20 @@
-// Implement models for Medication and Customer similar to User.js
+const express = require('express');
+const router = express.Router();
+const medicationController = require('../controllers/medicationController');
+
+// Query existing customers
+router.get('/', medicationController.getAllMedications);
+
+// Insert customer
+router.post('/', medicationController.createMedication);
+
+// Update customer details
+router.put('/:id', medicationController.updateMedication);
+
+// Soft delete customer record
+router.patch('/:id', medicationController.softDeleteMedication);
+
+// Delete customer record
+router.delete('/:id', medicationController.deleteMedication);
+
+module.exports = router;
