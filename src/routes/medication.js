3855bc6cd8ebd5819011d2d4792a3576eab3ac31
deelaka99 <123wejith@gmail.com>
2024-02-03 15:32:37 +0530

    added some changes to the medications routing msgs

--- src/routes/medication.js
+++ src/routes/medication.js
@@ -3,46 +3,46 @@ const router = express.Router();
 const medicationController = require("../controllers/medicationController");
 const authenticate = require("../middleware/authenticate");
 
-// Query existing customers
+// Query existing medications
 router.get("/", medicationController.getAllMedications);
 
-// Insert customer - Only owner can insert customers
+// Insert meeication - Only owner can insert medications
 router.post("/", authenticate, (req, res, next) => {
   if (req.user.role === "owner") {
     medicationController.createMedication(req, res, next);
   } else {
-    res.status(403).json({ message: "Only the owner can insert customers" });
+    res.status(403).json({ message: "Only the owner can insert medications" });
   }
 });
 
-// Update customer details - Only owner and managers can update customers
+// Update meeication details - Only owner and managers can update medications
 router.put("/:id", authenticate, (req, res, next) => {
   if (req.user.role === "owner" || req.user.role === "manager") {
     medicationController.updateMedication(req, res, next);
   } else {
     res
       .status(403)
-      .json({ message: "Only the owner and managers can update customers" });
+      .json({ message: "Only the owner and managers can update medications" });
   }
 });
 
-// Soft delete customer record - Only owner and managers can soft delete customers
+// Soft delete meeication record - Only owner and managers can soft delete medications
 router.patch("/:id", authenticate, (req, res, next) => {
   if (req.user.role === "owner" || req.user.role === "manager") {
     medicationController.softDeleteMedication(req, res, next);
   } else {
     res.status(403).json({
-      message: "Only the owner and managers can soft delete customers",
+      message: "Only the owner and managers can soft delete medications",
     });
   }
 });
 
-// Delete customer record - Only owner can permanently delete customers
+// Delete meeication record - Only owner can permanently delete medications
 router.delete("/:id", authenticate, (req, res, next) => {
   if (req.user.role === "owner") {
     medicationController.deleteMedication(req, res, next);
   } else {
-    res.status(403).json({ message: "Only the owner can delete customers" });
+    res.status(403).json({ message: "Only the owner can delete medications" });
   }
 });
 
